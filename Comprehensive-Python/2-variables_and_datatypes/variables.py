0379f22fb08f146b5ebe404831eb762f2367d93e
rahul2u <rsembedded@gmail.com>
2023-09-30 10:03:09 +0530

    variables

---
+++ Comprehensive-Python/2-variables_and_datatypes/variables.py
@@ -0,0 +1,40 @@
+import sys  # module
+# Module is used to pre writen code
+
+# variable
+my_name = "Rahul"
+my_age = 37
+print("Hello my name is {0} and age is {1}".format(my_name,my_age))
+print("Hello", my_name)
+# print function print the multiples value that is separated by Commas
+
+# comment
+# # single lien
+# ''' Multiline'''
+
+
+# generally 4  data types
+int_data = 20 # integers types
+float_data = 20.34 # float types
+com_data = 7 + 8j  # complex
+boolean_data = True # boolean
+boolean_data_new = False
+
+# float accuracy only 15 digits
+# data type casting
+
+print("Cast 1", type(int(5.6)))
+print("Cast 2", type(str(5.7)))
+print("Cast 3", type(chr(97))) # Unicode charter to string
+print("Cast 4", type(ord('a'))) # charter to unicode
+print("Cast 5", type(float(12)))
+
+# Unicode is every character in keyboard  is standardized a specific no
+
+
+
+
+
+
+
+
