a776842ce04759f780ac1b9b800b536638cbb950
rahulsingh50 <Kinghkhan@12345>
2024-04-06 14:52:20 +0530

    refactoring

---
+++ Comprehensive-Python/11-Python Basics 2/3. Truthy & Falsy.py
@@ -0,0 +1,21 @@
+# that is return false as boollen values
+
+empty_list = []
+empty_tuple = ()
+empty_set = set()
+empty_string = ''
+empty_range = range(0)
+zero_int = 0
+zero_float = 0.0
+zero_complex = complex(0j)
+none = None
+
+
+if none:
+    print ("Success or True")
+    print(none)
+else:
+    print("failure or False")
+    print(none)
+
+
